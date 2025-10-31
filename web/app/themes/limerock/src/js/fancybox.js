b7fb187afbe97733c0bafcd0ed175631dd40b834
Kate Kolisnyk <kate@jakeandco.com>
2025-10-31 17:30:51 +0200

    add fixes

--- web/app/themes/limerock/src/js/fancybox.js
+++ web/app/themes/limerock/src/js/fancybox.js
@@ -5,17 +5,6 @@ export function setup() {
     console.log('fancybox');
 
     Fancybox.bind("[data-fancybox]");
-
-    // document.addEventListener('fancybox:show', () => {
-    //   const vids = document.querySelectorAll('video[data-hover-play]');
-    //   vids.forEach(v => {
-    //     try {
-    //       v.pause();
-    //       v.currentTime = 0;
-    //       v.closest('.block_single-media-item')?.classList.remove('play');
-    //     } catch (e) { /* ignore */ }
-    //   });
-  // });
 }
 
 export function teardown() {
