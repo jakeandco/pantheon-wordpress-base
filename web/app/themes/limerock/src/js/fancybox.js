0d81cd9aa2ac0676e0162ab4a39e1e68bcff6381
Kate Kolisnyk <kate@jakeandco.com>
2025-11-05 12:07:54 +0200

    add fancybox styles updates

--- web/app/themes/limerock/src/js/fancybox.js
+++ web/app/themes/limerock/src/js/fancybox.js
@@ -2,9 +2,9 @@ import "@fancyapps/ui/dist/fancybox/fancybox.css";
 import { Fancybox } from "@fancyapps/ui";
 
 export function setup() {
-    console.log('fancybox');
+  console.log('fancybox');
 
-    Fancybox.bind("[data-fancybox]");
+  Fancybox.bind("[data-fancybox]");
 }
 
 export function teardown() {
