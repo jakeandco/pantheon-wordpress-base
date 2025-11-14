dc35cb6842e0223309cd11f7986d1d1225000dd0
Kate Kolisnyk <kate@jakeandco.com>
2025-11-14 15:29:30 +0200

    add featured-work-carousel styles

--- web/app/themes/limerock/views/blocks/featured-work-carousel/index.js
+++ web/app/themes/limerock/views/blocks/featured-work-carousel/index.js
@@ -1,4 +1,5 @@
 import { Swiper } from "swiper";
+import { Autoplay } from "swiper/modules";
 import { Navigation } from "swiper/modules";
 
 export function setup() {
@@ -7,11 +8,13 @@ export function setup() {
   );
   if (!selectedBlocks.length) return;
 
+  console.log('Featured Work Carousel Block js');
   const featuredWorkSwiper = new Swiper(".featured-post-swiper", {
     modules: [Autoplay, Navigation],
     direction: "horizontal",
     effect: "slide",
-    slidesPerView: "auto",
+    slidesPerView: 1,
+    loop: false,
     // Navigation arrows
     navigation: {
       nextEl: ".featured-work-swiper-button-next",
